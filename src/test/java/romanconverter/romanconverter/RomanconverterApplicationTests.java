84a6de8df9ee4f5314e75b7f101f0b224fa61d62
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2021-12-22 12:30:14 -0300

    conversor de numero romano para numero inteiro

---
+++ src/test/java/romanconverter/romanconverter/RomanconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package romanconverter.romanconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
